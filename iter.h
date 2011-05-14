ed80662224807d258080130d30fde20f614aa10a
Toshakins <tonik_v_o@list.ru>
2011-05-14 18:00:51 +0300

    check point commit

--- iter.h
+++ iter.h
@@ -7,6 +7,10 @@ class Mark {
 public:
 	//(un)marked columns, rows
 	vector <int> markedC, unmarkedC, markedR, unmarkedR;
+	vector<vector<int> > mmark;
+	Mark() {
+		//mmark.resize()
+	}
 	void ing() {
 		for (UINT i = sellers.size(); i >= 0; --i) {
 			if (disrepancy.column[i] == 0) {
@@ -22,7 +26,13 @@ public:
 
 bool search_stage() {
 	for (UINT i = 0; i < mark.unmarkedC.size(); i++) {
-
+		for (UINT j = 0; j < customers.size(); j++) {
+			if (C[j][mark.unmarkedC[i]] == 0){
+				if (disrepancy.row[j] > 0) return 1; //call correction of X
+				mark.markedR.push_back(j);
+				remove(mark.unmarkedR.begin(), mark.unmarkedR.end(), j);
+			}
+		}
 	}
 	return 0;
 }
